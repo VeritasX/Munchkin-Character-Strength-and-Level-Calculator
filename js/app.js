7f8b27122c5f4ec7770a77c8a30bc250c1549734
Aaron Endsley <aaronendsley@gmail.com>
2016-02-07 10:05:28 -0600

    updated the app.js file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 /**
  * Created by aaronendsley on 2/7/16.
  * Munchkin Character stregth and level calculator
+ *
+ *
+ * This app will:
+ * Will allow you to select your deck
+ * Search cards and add them to your in play to add them to your strength
+ * allow you to add your class cards
+ * allow you to add you weapon cards
+ * 
+ *
  */
 'use strict'
+
+
+
