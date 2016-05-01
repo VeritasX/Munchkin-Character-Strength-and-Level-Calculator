3195a3e38fa1e7c441c7a8b011b9a666308e4fa4
Aaron Endsley <aaronendsley@gmail.com>
2016-05-01 08:39:32 -0500

    added some missed files that were not commited previously

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+/**
+ * Created by aaronendsley on 4/30/16.
+ */
+'use strict'
+
+        var gulp=require('gulp');
+        var gulpConcat=require('gulp-concat');
+        var gulpBabel = require('gulp-babel');
+        var gulpuglify = require('gulp-uglify');
+        var gulpSass = require('gulp-sass');
+        var gulpSass = require('gulp-sass');
+
+
