0779c1566dbb015d4e9e388b4f1dcfc200384db0
Aaron Endsley <aaronendsley@gmail.com>
2016-05-08 08:03:09 -0500

    Added comments pertaing to ideas for ui functionality, and editied the spacing of the code

--- js/functionality.js
+++ js/functionality.js
@@ -1,78 +1,85 @@
-/**
- * Created by aaronendsley on 5/6/16.
- */
-'use strict'
+    /**
+     * Created by aaronendsley on 5/6/16.
+     */
+    'use strict'
 
-var getMyCard=function(card){
-    var myCard=card;
-    for(var cards in deck){
-        if(myCard == deck[cards].cardName){
-            console.log(deck[cards].cardName);
-            console.log(deck[cards].cardType);
-            console.log(deck[cards].points);
-            console.log(deck[cards].runAway);
-            console.log(deck[cards].cardValue);
+    var getMyCard=function(card){
+        var myCard=card;
+        for(var cards in deck){
+            if(myCard == deck[cards].cardName){
+                console.log(deck[cards].cardName);
+                console.log(deck[cards].cardType);
+                console.log(deck[cards].points);
+                console.log(deck[cards].runAway);
+                console.log(deck[cards].cardValue);
+    
+                usersCard=deck[cards];
+            }else{
+                console.log('That card does not exist in the deck');
+            }
+        }
 
-            usersCard=deck[cards];
-        }else{
-            console.log('That card does not exist in the deck');
+        //Not Sure if the UI will have to be dynamically changed when this is called, still deciding
+    };
+
+    var addToUser=function(){
+        userDeck.push(usersCard);
+        usersCard.points=parseInt(usersCard.points);
+        userStats.points=parseInt(userStats.points);
+        userStats.runAway=parseInt(userStats.runAway);
+        usersCard.runAway=parseInt(usersCard.runAway);
+        usersCard.cardValue=parseInt(usersCard.cardValue);
+        userStats.totalWorth=parseInt(userStats.totalWorth);
+        userStats.points=userStats.points + usersCard.points;
+        userStats.totalWorth=userStats.totalWorth + usersCard.cardValue;
+        userStats.runAway+=usersCard.runAway;
+
+        if(usersCard.cardType !== 'race') {
+            userStats.gear.push(usersCard.cardName);
+        }
+        usersCard='';
+
+        //Each call of this will have to reload all the ui elements being effect by this data
+    };
+
+    var removeFromUser=function(card){
+        var removeCard=card;
+        for(var i=0; i < userDeck.length; i++){
+            if(userDeck[i].cardName == card){
+                console.log(userDeck[i].cardName + ' will be removed')
+                userDeck.splice(i);
+
+            }
+        }
+        //each call of removeFromUser will have the argument passed in dynamically
+        //each call will also have to reload the list of userDeck and other related items when the ui is added
+
+    };
+
+    var goUpALevel=function(){
+        userStats.level+=1;
+        if(userStats.level === 10){
+            alert('You won the game now no one likes you...jk');
+            //The Ui will have to be dynamically changed when this occurs
         }
     }
-};
-var addToUser=function(){
-    userDeck.push(usersCard);
-    usersCard.points=parseInt(usersCard.points);
-    userStats.points=parseInt(userStats.points);
-    userStats.runAway=parseInt(userStats.runAway);
-    usersCard.runAway=parseInt(usersCard.runAway);
-    usersCard.cardValue=parseInt(usersCard.cardValue);
-    userStats.totalWorth=parseInt(userStats.totalWorth);
-    userStats.points=userStats.points + usersCard.points;
-    userStats.totalWorth=userStats.totalWorth + usersCard.cardValue;
-    userStats.runAway+=usersCard.runAway;
-    
-    if(usersCard.cardType !== 'race') {
-        userStats.gear.push(usersCard.cardName);
+
+    var goDownALevel=function(){
+        userStats.level-=1;
+        //The UI for the level will have to be dynamically changed when this function is called
     }
-    usersCard='';
-};
 
+    var usersCard={};
 
-var removeFromUser=function(card){
-    var removeCard=card;
-    for(var i=0; i < userDeck.length; i++){
-        if(userDeck[i].cardName == card){
-            console.log(userDeck[i].cardName + ' will be removed')
-            userDeck.splice(i);
+    var userDeck=[];
 
-        }
-    }
-    //each call of removeFromUser will have the argument passed in dynamically
-    
-};
-
-var goUpALevel=function(){
-    userStats.level+=1;
-    if(userStats.level === 10){
-        alert('You won the game now no one likes you...jk');
-    }
-}
-
-var goDownALevel=function(){
-    userStats.level-=1;
-}
-
-var usersCard={};
-
-var userDeck=[];
-
-var userStats={
-    name:'',
-    level:1,
-    points:0,
-    gear:[],
-    runAway:0,
-    totalWorth:0
-};
+    var userStats={
+        name:'',
+        level:1,
+        points:0,
+        gear:[],
+        runAway:0,
+        totalWorth:0
+    };
 
 
