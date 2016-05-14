dcc35990e1e475dfc06ae429e14d376715b4b9c4
Aaron Endsley <aaronendsley@gmail.com>
2016-05-14 10:09:09 -0500

    added integrated the card selection functionality into the ui

--- js/app.js
+++ js/app.js
@@ -3,14 +3,31 @@
  */
 'use strict';
 var setUpDecks={
+    hideElements:function(id){
+        $(id).hide();
+    },
     legends:legends()
+
 };
 
 
 var selectDeck=function(){
     var getValue=$('.pickDeck').val();
+    var thisSection=$('.selectCard');
+    var setUpCardSelection=function(){
+        
+            if(deck !== {}){
+                for(var card in deck){
+                    $('#cardChooser').append('<option value=' +deck[card].cardName +'>'+deck[card].cardName+'</option>')
+                };
+            };
+        
+    };
+
 
     setUpDecks[getValue];
+    setUpCardSelection();
+
 };
 
 var selectCard=function(){
