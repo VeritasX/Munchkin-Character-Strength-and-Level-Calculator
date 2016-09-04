2bfd959f212370bc78beb20ebaebc485b40cabbe
Aaron Endsley <aaronendsley@gmail.com>
2016-09-03 21:26:21 -0500

    small changes

--- js/uimethods.js
+++ js/uimethods.js
@@ -2,32 +2,32 @@
  * Created by aaronendsley on 5/14/16.
  */
 'use strict';
-var setUpDecks={
-    hideElements:function(id) {
-        var itemId=$(id);
+var setUpDecks = {
+    hideElements: function(id) {
+        var itemId = $(id);
         itemId.slideToggle(100);
     },
-    userRefresh: function(){
+    userRefresh: function() {
         $('.userDisplay').remove();
         $('.userCards').remove();
         $('.removeCardsFromDeck').empty();
-            $('.stats').append('<div class="userDisplay"></div>');
+        $('.stats').append('<div class="userDisplay"></div>');
         $('.cardsInPlayContainer').append('<div class="userCards"></div>');
         $('.removeCardsFromDeck').append('<optgroup label="cards in your deck" class="cardsForRemoval"><option>Choose Your Card</option></optgroup>');
-            for(var stat in userStats){
-                    if(userStats[stat] === userStats.name) {
-                        $('.userDisplay').append('<p><strong> User:</strong>' + userStats[stat] + '</p>');
-                    }else if(userStats[stat] !== userStats.name && userStats[stat] !== userStats.totalWorth) {
-                        $('.userDisplay').append('<p><strong>' + titles[stat] + ':</strong>' + userStats[stat] + '</p>');
-                    }else if(userStats[stat] === userStats.totalWorth && userStats.totalWorth !== 0){
-                        $('.userDisplay').append('<p><strong> Total Gold Worth:</strong>' + userStats[stat] + '</p>');
-                    };
+        for (var stat in userStats) {
+            if (userStats[stat] === userStats.name) {
+                $('.userDisplay').append('<p><strong> User:</strong>' + userStats[stat] + '</p>');
+            } else if (userStats[stat] !== userStats.name && userStats[stat] !== userStats.totalWorth) {
+                $('.userDisplay').append('<p><strong>' + titles[stat] + ':</strong>' + userStats[stat] + '</p>');
+            } else if (userStats[stat] === userStats.totalWorth && userStats.totalWorth !== 0) {
+                $('.userDisplay').append('<p><strong> Total Gold Worth:</strong>' + userStats[stat] + '</p>');
             };
-            for(var i =0; i < userDeck.length; i++){
-                $('.userCards').append('<p>' + userDeck[i].cardName +'</p>');
-            };
-           for(var i =0; i < userDeck.length; i++){
-             $('.removeCardsFromDeck').append('<option value=' +userDeck[i].id +'>'+ userDeck[i].cardName+'</option>');
-           }
+        };
+        for (var i = 0; i < userDeck.length; i++) {
+            $('.userCards').append('<p>' + userDeck[i].cardName + '</p>');
+        };
+        for (var i = 0; i < userDeck.length; i++) {
+            $('.removeCardsFromDeck').append('<option value=' + userDeck[i].id + '>' + userDeck[i].cardName + '</option>');
         }
+    }
 };
